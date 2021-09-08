9cd1d535e18bff2763cf51cc9b873f174f03a665
marc572m <marc572m@edu.easj.dk>
2021-09-08 11:26:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int test;
+
+        System.out.println(test);
+
+        //Value = Boo!7
+        System.out.println("Boo!"+7);
+
+
+    }
+}
